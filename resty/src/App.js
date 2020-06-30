e3ab3d80829abd42b142bde16c5346ceadcb8f35
X-Anas-X <anas.zain1996@gmail.com>
2020-07-01 00:22:42 +0300

    done

--- resty/src/App.js
+++ resty/src/App.js
@@ -2,18 +2,34 @@ import React from 'react';
 import Header from './component/Header'
 import Footer from './component/Footer'
 import Main from './component/Main'
+import Results from './component/Results'
 import './component/css/App.scss';
 import './component/css/reset.css'
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: {},
+    };
+  }
 
-function App() {
+  handleForm = (results) => {
+    this.setState({ result: results });
+  };
+
+
+
+render() {
   return (
     <React.Fragment>
       <Header />
-      <Main />
+      <Main handler = {this.handleForm}/>
+      <Results result = {this.state.result}/>
       <Footer />
     </React.Fragment>
-  ) 
+  );
+ }
 
 }
 
--- resty/src/component/Main.js
+++ resty/src/component/Main.js
@@ -2,24 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import './css/main.scss'
 
+import './Results'
+
 class Main extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            words: '',
-            method: ''
-        }
-    }
+            url: '',
+            method: '',
+            request: {}
 
-    handleWord = e =>{
-        let words = e.target.value;
-        this.setState({words});
+        };
+    }
+// **********************************************
+handleSubmit = async e => {
+    // fetch data form API 
+    e.preventDefault();
+    let url = this.state.url;
+    let raw = await fetch(url); // star wars API
+    let data = await raw.json();
+    let results = {
+        Headers: raw.headers,
+        Response: data
+    }
+    this.props.handler(results)
+}
+
+
+
+// **********************************
+
+
+    handleUrl = e =>{
+        let url = e.target.value;
+        this.setState({url});
     }
 
     handleClick = e =>{
         e.preventDefault();
         let results = document.getElementById('div2')
-        let words = <div>{this.state.method} <span>{this.state.words}</span> </div>
+        let words = <div>{this.state.method} <span>{this.state.url}</span> </div>
         ReactDOM.render(words, results);
     }
 
@@ -32,20 +54,21 @@ class Main extends React.Component {
     render() {
         return (
             <section>
-                <form>
+                <form onSubmit={this.handleSubmit}>
 
             <div class="div1">
                 <h2>URL:</h2>
+                
    
-                <input type="url" id="url" onChange={this.handleWord} />
-                <button onClick={this.handleClick} >GO!</button>
+                <input type="url" id="url" onChange={this.handleUrl} />
+                <button type="submit" >GO!</button>
             </div>
 
             <div class="div3">
-               <input type="radio" name="method" onClick={this.handleMethod} value="get"/>GET
-               <input type="radio" name="method" onClick={this.handleMethod} value="post"/>POST
-               <input type="radio" name="method" onClick={this.handleMethod} value="put"/>PUT
-               <input type="radio" name="method" onClick={this.handleMethod} value="delete"/>DELETE
+               <input type="radio" name="method" onChange={this.handleMethod} value="get"/>GET
+               <input type="radio" name="method" onChange={this.handleMethod} value="post"/>POST
+               <input type="radio" name="method" onChange={this.handleMethod} value="put"/>PUT
+               <input type="radio" name="method" onChange={this.handleMethod} value="delete"/>DELETE
             </div>
 
             <div id="div2">
